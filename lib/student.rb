93bd296ad1e55bb748d6ca1f4e132f944835099f
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-03-08 18:40:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,15 +117,15 @@ class Student
   end
 
   def self.all_students_in_grade_X(number)
-    sql = <<-SQL 
-      SELECT* 
-      FROM students 
+    sql = <<-SQL
+      SELECT*
+      FROM students
       WHERE grade = ?
-    SQL 
+    SQL
 
     DB[:conn].execute(sql, number).map do |row|
       self.new_from_db(row)
     end
-  end 
+  end
 
 end
